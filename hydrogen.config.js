e525b674fdedc7992e4a2b969fa93b926d59c797
Farhan71 <farhanitnstu@gmail.com>
2022-11-20 22:09:20 +0600

    start-hydrogen-app

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'hydrogen-storefront-app.myshopify.com',
+    storefrontToken: 'af9de22478e0d9f8ddcde1e947a89794',
     storefrontApiVersion: '2022-07',
   },
 });
